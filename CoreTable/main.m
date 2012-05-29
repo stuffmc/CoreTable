f83763dd84ed124e24b7fdc3f3cfdde04719af16
StuFF mc <mc@stuffmc.com>
2012-05-29 10:53:47 +0200

    Initial Commit

---
+++ CoreTable/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  CoreTable
+//
+//  Created by StuFF mc on 5/29/12.
+//  Copyright (c) 2012 StuFF mc. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc, (const char **)argv);
+}
